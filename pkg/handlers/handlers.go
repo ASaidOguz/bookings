e77ccef9a43f701216cd72997c57418e495b6be3
ASaidOguz <ahmetsaid2136@gmail.com>
2022-03-26 17:21:29 +0300

    Cut the html source files into go templates and created new page templates and  handlers

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -49,3 +49,41 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 		StringMap: stringMap,
 	})
 }
+
+//Availability is the the search-availability  page handler
+func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
+	remoteIp := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIp)
+
+	render.RenderTemplate(w, "search-availability.page.html", &models.TemplateData{})
+}
+
+//Generals is the the Generals room page handler
+func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
+	remoteIp := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIp)
+
+	render.RenderTemplate(w, "generals.page.html", &models.TemplateData{})
+}
+
+//Majors is the the Majors room page handler
+func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
+	remoteIp := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIp)
+
+	render.RenderTemplate(w, "majors.page.html", &models.TemplateData{})
+}
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	remoteIp := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIp)
+
+	render.RenderTemplate(w, "contact.page.html", &models.TemplateData{})
+}
+
+//Reservation is the make-reservation page handler
+func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	remoteIp := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIp)
+
+	render.RenderTemplate(w, "make-reservation.page.html", &models.TemplateData{})
+}
