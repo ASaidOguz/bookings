d42c20630fc83f4390ee7a3754711958e646b61a
ASaidOguz <ahmetsaid2136@gmail.com>
2022-03-26 23:55:28 +0300

    CsrfToken added to td(Template Data)

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/ASaidOguz/bookings/pkg/config"
 	"github.com/ASaidOguz/bookings/pkg/models"
+	"github.com/justinas/nosurf"
 )
 
 var functions = template.FuncMap{}
@@ -18,12 +19,13 @@ var app *config.AppConfig
 func NewTemplate(a *config.AppConfig) {
 	app = a
 }
-func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData {
+	td.CSRFToken = nosurf.Token(r)
 	return td
 }
 
 // RenderTemplate renders templates using html/template
-func RenderTemplate(w http.ResponseWriter, html string, td *models.TemplateData) {
+func RenderTemplate(w http.ResponseWriter, r *http.Request, html string, td *models.TemplateData) {
 
 	var tc map[string]*template.Template
 	//Development mode on-off
@@ -41,7 +43,7 @@ func RenderTemplate(w http.ResponseWriter, html string, td *models.TemplateData)
 	}
 
 	buf := new(bytes.Buffer)
-	td = AddDefaultData(td)
+	td = AddDefaultData(td, r)
 	_ = t.Execute(buf, td)
 	_, err := buf.WriteTo(w)
 	if err != nil {
