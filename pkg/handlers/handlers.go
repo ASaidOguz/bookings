bf1e9cd0e08e4103a6f59880204712dda7ab7d12
ASaidOguz <ahmetsaid2136@gmail.com>
2022-03-27 02:12:50 +0300

    added jsonResponse type and populated .Added AvailabilityJSON handler to send json data with  header

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,7 +1,9 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/ASaidOguz/bookings/pkg/config"
@@ -68,6 +70,26 @@ func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
 	end := r.Form.Get("end")
 
 	w.Write([]byte(fmt.Sprintf("Start date is :%s ,End date is :%s...", start, end)))
+}
+
+type jsonResponse struct {
+	OK      bool   `json:"ok"`
+	Message string `json:"message"`
+}
+
+//AvailabilityJSON handles request for availability and sends JSON responds
+func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
+	resp := jsonResponse{
+		OK:      false,
+		Message: "Available!",
+	}
+	out, err := json.MarshalIndent(resp, "", "     ")
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println(string(out))
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
 }
 
 //Generals is the the Generals room page handler
