ab63e1497353e3841ae47e351b6068a8ea1ba57e
ASaidOguz <ahmetsaid2136@gmail.com>
2022-04-08 01:26:27 +0300

    added empty reservation varible,required minlength and  Isemail functions initiated

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -122,8 +122,13 @@ func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
 	remoteIp := r.RemoteAddr
 	m.App.Session.Put(r.Context(), "remote_ip", remoteIp)
 
+	var emptyReservation models.Reservation
+	data := make(map[string]interface{})
+	data["reservation"] = emptyReservation
+
 	render.RenderTemplate(w, r, "make-reservation.page.html", &models.TemplateData{
 		Form: forms.New(nil),
+		Data: data,
 	})
 }
 
@@ -145,8 +150,10 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 	}
 	form := forms.New(r.PostForm)
 
-	form.Has("first_name", r)
-
+	//form.Has("first_name", r)
+	form.Required("first_name", "last_name", "email")
+	form.MinLength("first_name", 3, r)
+	form.Isemail("email")
 	if !form.Valid() {
 		data := make(map[string]interface{})
 		data["reservation"] = reservation
