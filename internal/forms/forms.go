5874ac71adee2ecbee5546bcc4db40a9c8bd7ad4
ASaidOguz <ahmetsaid2136@gmail.com>
2022-04-08 01:25:26 +0300

    Required ,Minlength and Isemail functions added ,put check box on has ^^

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -1,8 +1,12 @@
 package forms
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
+
+	"github.com/asaskevich/govalidator"
 )
 
 type Form struct {
@@ -24,12 +28,40 @@ func New(data url.Values) *Form {
 
 }
 
-//Has check if form field is in post and not empty
+//Required check for required fields.
+func (f *Form) Required(fields ...string) {
+	for _, field := range fields {
+		value := f.Get(field)
+		if strings.TrimSpace(value) == "" {
+			f.Errors.Add(field, "This field cannot be blank")
+
+		}
+	}
+}
+
+//MinLength checks strings minimum length
+func (f *Form) MinLength(field string, length int, r *http.Request) bool {
+	x := r.Form.Get(field)
+	if len(x) < length {
+		f.Errors.Add(field, fmt.Sprintf("This field has to at least %d character long", length))
+		return false
+	}
+	return true
+}
+
+//Isemail checks the field if its valid email form
+func (f *Form) Isemail(field string) {
+	if !govalidator.IsEmail(f.Get(field)) {
+		f.Errors.Add(field, "Invalid email address")
+	}
+}
+
+//Has check if form field is in post and not empty useful function for check box methods!!!!
 func (f *Form) Has(field string, r *http.Request) bool {
 
 	x := r.Form.Get(field)
 	if x == "" {
-		f.Errors.Add(field, "This field cant be blank")
+
 		return false
 	}
 	return true
