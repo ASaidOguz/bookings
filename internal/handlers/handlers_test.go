c3217fd292a26a7030ccc25c94f13da14d67641a
ASaidOguz <ahmetsaid2136@gmail.com>
2022-04-12 00:44:54 +0300

    handler test intiated succesfully

---
+++ internal/handlers/handlers_test.go
@@ -0,0 +1,54 @@
+package handlers
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type postData struct {
+	key   string
+	value string
+}
+
+var TheTests = []struct {
+	name               string
+	url                string
+	method             string
+	params             []postData
+	expectedStatusCode int
+}{
+
+	{"home", "/", "GET", []postData{}, http.StatusOK},
+	{"about", "/about", "GET", []postData{}, http.StatusOK},
+	{"gq", "/generals-quarters", "GET", []postData{}, http.StatusOK},
+	{"ms", "/majors-suite", "GET", []postData{}, http.StatusOK},
+	{"sa", "/search-availability", "GET", []postData{}, http.StatusOK},
+	{"con", "/contact", "GET", []postData{}, http.StatusOK},
+	{"mr", "/make-reservation", "GET", []postData{}, http.StatusOK},
+}
+
+func TestHandlers(t *testing.T) {
+
+	routes := getRoots()
+	ts := httptest.NewTLSServer(routes)
+	defer ts.Close()
+
+	for _, e := range TheTests {
+		if e.method == "GET" {
+			resp, err := ts.Client().Get(ts.URL + e.url)
+			if err != nil {
+				t.Log(err)
+				t.Fatal(err)
+			}
+
+			if resp.StatusCode != e.expectedStatusCode {
+				t.Errorf("for %s ,expected %d but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
+			}
+
+		} else {
+
+		}
+	}
+
+}
