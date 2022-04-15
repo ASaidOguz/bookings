987ef1a2f9a3ccb19c2d68ce0707359d50629d87
ASaidOguz <ahmetsaid2136@gmail.com>
2022-04-15 16:00:35 +0300

    Get function modified so it will check the correct form

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -2,7 +2,6 @@ package forms
 
 import (
 	"fmt"
-	"net/http"
 	"net/url"
 	"strings"
 
@@ -40,8 +39,8 @@ func (f *Form) Required(fields ...string) {
 }
 
 //MinLength checks strings minimum length
-func (f *Form) MinLength(field string, length int, r *http.Request) bool {
-	x := r.Form.Get(field)
+func (f *Form) MinLength(field string, length int) bool {
+	x := f.Get(field)
 	if len(x) < length {
 		f.Errors.Add(field, fmt.Sprintf("This field has to at least %d character long", length))
 		return false
@@ -50,16 +49,18 @@ func (f *Form) MinLength(field string, length int, r *http.Request) bool {
 }
 
 //Isemail checks the field if its valid email form
-func (f *Form) Isemail(field string) {
+func (f *Form) Isemail(field string) bool {
 	if !govalidator.IsEmail(f.Get(field)) {
 		f.Errors.Add(field, "Invalid email address")
+		return false
 	}
+	return true
 }
 
 //Has check if form field is in post and not empty useful function for check box methods!!!!
-func (f *Form) Has(field string, r *http.Request) bool {
+func (f *Form) Has(field string) bool {
 
-	x := r.Form.Get(field)
+	x := f.Get(field)
 	if x == "" {
 
 		return false
