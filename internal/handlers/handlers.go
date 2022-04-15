fef73a886c9d66584b7ec1a8f040ff86da8e0a1e
ASaidOguz <ahmetsaid2136@gmail.com>
2022-04-15 16:01:34 +0300

    removed r request from minlenght function

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -152,7 +152,7 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 
 	//form.Has("first_name", r)
 	form.Required("first_name", "last_name", "email")
-	form.MinLength("first_name", 3, r)
+	form.MinLength("first_name", 3)
 	form.Isemail("email")
 	if !form.Valid() {
 		data := make(map[string]interface{})
